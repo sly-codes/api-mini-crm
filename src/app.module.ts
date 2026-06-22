16bfbccca59ae7f56bb2ce99107b4edf8599ac7c
Issa Koné <kissadev10@gmail.com>
2026-06-22 00:59:56 +0000

    @nestjs/config and joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { CompaniesModule } from './modules/companies/companies.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ContactsModule } from './modules/contacts/contacts.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { OpportunitiesModule } from './modules/opportunities/opportunities.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
     PrismaModule,
     AuthModule,
     CompaniesModule,
